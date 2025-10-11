b22e7efca8a7545ad1ecf5ce4e10aa87ea34e9f3
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-11 13:34:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Serveur actif sur le port ${PORT}`));
