4c68c3a4eb4b678d920af379b273a11950dba690
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-12 21:50:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,21 @@
-const CACHE_NAME = "synthese-prod-v15.3";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./style.css",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./Lactalis2023Logo.svg"
-];
-
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open('synthese-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
