fbc6acbd7258a0462fcadfaa356ba95ea5b90487
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-12 19:31:10 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,31 @@
-const CACHE_NAME="synthese-cache-v4";
-const FILES=[
-  "index.html","style.css","app.js","manifest.json",
-  "Lactalis2023Logo.svg","icons/icon-192.png","icons/icon-512.png"
+const CACHE_NAME = "synthese-prod-v15.3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./style.css",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./Lactalis2023Logo.svg"
 ];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
-  self.skipWaiting();
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("activate",e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));
-  self.clients.claim();
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
 
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const clone=r.clone();
-    caches.open(CACHE_NAME).then(c=>c.put(e.request,clone));
-    return r;
-  }).catch(()=>caches.match(e.request)));
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
