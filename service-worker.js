ad83040a7cd176a53ee683b095ca60f591784101
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-12 14:26:43 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,46 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('synthese-cache-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './app.js',
-        './style.css',
-        './manifest.json',
-        './icons/icon-192.png',
-        './icons/icon-512.png'
-      ]);
+const CACHE_NAME = "synthese-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./icon-512.png",
+  "./icon-192.png",
+  "./Lactalis2023Logo.svg",
+  "https://cdn.jsdelivr.net/npm/chart.js"
+];
+
+// Installation du service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+// Activation et nettoyage du cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Interception des requêtes réseau
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
