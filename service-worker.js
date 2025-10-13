b8dd3b1916c56845a2dc8b0fb0cc1a7b7164665a
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-13 18:09:01 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,46 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('synthese-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './app.js',
-        './manifest.json',
-        './icons/icon-192.png',
-        './icons/icon-512.png'
-      ]);
-    })
+// Service Worker - Synthèse Production V19
+
+const CACHE_NAME = "synthese-production-v19";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./Lactalis2023Logo.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Installation : mise en cache des fichiers essentiels
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+// Activation : nettoyage ancien cache
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response ||
+      fetch(event.request).then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      })
+    ).catch(() => caches.match("./index.html"))
   );
 });
