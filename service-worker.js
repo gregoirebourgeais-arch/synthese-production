2d295dd4d8ad172282f6043a2ff81934e747fc70
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-13 19:29:15 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,37 @@
-// Service Worker - Synthèse Production V19
-
 const CACHE_NAME = "synthese-production-v19";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./Lactalis2023Logo.svg",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "Lactalis2023Logo.svg",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
-// Installation : mise en cache des fichiers essentiels
+// Installation du service worker
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
+  console.log("✅ Service Worker installé !");
 });
 
-// Activation : nettoyage ancien cache
+// Activation (nettoyage anciens caches)
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then(keys => 
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
-  self.clients.claim();
+  console.log("🧹 Service Worker activé !");
 });
 
-// Interception des requêtes
+// Interception des requêtes réseau
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response ||
-      fetch(event.request).then(res => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, res.clone());
-          return res;
-        });
-      })
-    ).catch(() => caches.match("./index.html"))
+    caches.match(event.request).then(resp => 
+      resp || fetch(event.request).catch(() => caches.match("index.html"))
+    )
   );
 });
