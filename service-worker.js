2aa5a1d9fb6073bf13ce12dc07514747ed548851
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-15 20:59:10 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,36 @@
-const CACHE_NAME = "synthese-production-v38.2";
+const CACHE_NAME = "synthese-v38-3";
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
-// INSTALLATION DU SERVICE WORKER
+// Installation : mise en cache initiale
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Mise en cache initiale réussie.");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// ACTIVATION
+// Activation : nettoyage ancien cache
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// MODE HORS LIGNE
+// Récupération depuis le cache
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
