06bd2285ac943723226f64e91781389e386af408
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-15 21:29:54 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,23 @@
-const CACHE_NAME = "synthese-v38-3";
-const FILES_TO_CACHE = [
-  "index.html",
-  "style.css",
-  "app.js",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
+const CACHE_NAME = 'synthese-prod-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './logo.png'
 ];
 
-// Installation : mise en cache initiale
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// Activation : nettoyage ancien cache
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-// Récupération depuis le cache
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
