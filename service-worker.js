359910b8f7828bf9af8d9fc91d51a22810a58ed8
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-16 19:59:43 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,38 @@
-const CACHE_NAME = "synthese-lactalis-v1";
+const CACHE_NAME = "synthese-atelier-v1";
 const urlsToCache = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
   "./manifest.json",
+  "./icons/icon-72.png",
+  "./icons/icon-96.png",
+  "./icons/icon-144.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
-// Installation du Service Worker
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activation
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-// Interception des requêtes
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
   );
 });
